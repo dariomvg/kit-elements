2335c0babaa55d4f10a98b21d22bb147da6f1784
dariomvg <contact.dario.info@gmail.com>
2024-11-22 10:20:07 -0300

    eliminate const in format-dates

--- format-dates/src/get_formats.ts
+++ format-dates/src/get_formats.ts
@@ -2,24 +2,20 @@ import { TypeDays, TypeMonths } from "./types";
 import { days } from "./utils/days";
 import { months } from "./utils/months";
 
-
 export const getFirstDate = (): string => {
   const date = new Date();
-  const newDate: string = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-  return newDate;
+  return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 };
 
 export const getSecondDate = (): string => {
   const date = new Date();
   const month: TypeMonths[] = months.filter((month) => month.id === date.getMonth());
-  const newDate: string = `${date.getDate()} ${month[0].short} ${date.getFullYear()}`;
-  return newDate;
+  return `${date.getDate()} ${month[0].short} ${date.getFullYear()}`;
 };
 
 export const getShortDate = (): string => {
   const date = new Date();
-  const newDate: string = `${date.getDate()}/${date.getMonth()+1}`;
-  return newDate;
+  return `${date.getDate()}/${date.getMonth()+1}`;
 };
 
 export const getCompleteDate = (format: string = "complete"): string => {
@@ -28,17 +24,14 @@ export const getCompleteDate = (format: string = "complete"): string => {
   const month: TypeMonths[] = months.filter((month) => month.id === date.getMonth());
 
   if (format === "short") {
-    const newDate: string = `${day[0].short}, ${date.getDate()} ${month[0].month}`;
-    return newDate;
+    return `${day[0].short}, ${date.getDate()} ${month[0].month}`;
   }
 
-  const newDate: string = `${day[0].day}, ${month[0].month} ${date.getDate()}, ${date.getFullYear()}`;
-  return newDate;
+  return `${day[0].day}, ${month[0].month} ${date.getDate()}, ${date.getFullYear()}`;
 };
 
 export const getLocalHour = (): string => {
   const date = new Date();
-  const newDate = `${date.getHours()}:${date.getMinutes()}`;
-  return newDate;
+  return `${date.getHours()}:${date.getMinutes()}`;
 };
 
